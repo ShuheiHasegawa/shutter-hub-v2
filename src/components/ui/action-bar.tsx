bcbea1096a12b8193ea02d99a3b59efde89c274e
Shuhei Hasegawa <shuhei.hasegawa1989@gmail.com>
2025-06-19 09:55:58 +0900

    fix: アクションバーの幅をサイドバー考慮で修正し確実な中央寄せを実現

--- src/components/ui/action-bar.tsx
+++ src/components/ui/action-bar.tsx
@@ -53,9 +53,11 @@ export function ActionBar({
   return (
     <div
       className={cn(
-        'w-full border-t',
+        'border-t',
         sticky && 'fixed bottom-0 right-0 z-40',
         sticky && 'left-0 md:left-64',
+        sticky && 'w-full md:w-[calc(100%-16rem)]', // サイドバー幅（16rem = 256px）を考慮
+        !sticky && 'w-full',
         backgroundClasses[background],
         className
       )}
